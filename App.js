38ab3ac732de074175a281aacd08e5159e689700
dorado5115 <doradodx@gmail.com>
2022-03-31 15:19:17 -0600

    program

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Animated } from 'react-native';
 
 export default function App() {
+  const color = new Animated.Value(0);
+
+  const changeColor = () => {
+    Animated.timing(color, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: false
+    }).start(() => {
+      color.setValue(0);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Animated.View style={[styles.text, { backgroundColor: color.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['red', 'green']
+      })}]} >
+        <Text style={styles.text} >Alooo policia</Text>
+      </Animated.View>
+      <Button title="Change Color" onPress={changeColor} />
     </View>
   );
 }
@@ -16,5 +34,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  box: {
+    width: 100,
+    height: 100,
+  },
+
+  text: {
+    fontSize: 60,
+    color: 'red',
   },
 });
